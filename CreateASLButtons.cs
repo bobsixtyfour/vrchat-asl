241b02db204759cb94014fc400e5db54c391760b
bobsixtyfour <61665677+bobsixtyfour@users.noreply.github.com>
2020-03-05 00:14:41 -0500

    Add files via upload

--- CreateASLButtons.cs
+++ CreateASLButtons.cs
@@ -72,17 +72,134 @@ public class CreateASLButtons : MonoBehaviour {
 			{"I (Me)","Bob64"},{"My","Bob64"},{"Your","Bob64"},{"His","Bob64"},{"Her","Bob64"},{"We","Bob64"},{"They","Bob64"},{"Their","Bob64"},{"Over there","Bob64"},{"Our","Bob64"},{"It's","Bob64"},
 			{"Inside","Bob64"},{"Outside","Bob64"},{"Hidden","Bob64"},{"Behind","Bob64"},{"Above","Bob64"},{"Below","Bob64"},{"Here","Bob64"},{"Beside","Bob64"},{"Back","Bob64"},{"Front","Bob64"},
 			{"Who","Bob64"},{"Where","Bob64"},{"When","Bob64"},{"Why","Bob64"},{"Which","Bob64"},{"What","Bob64"},{"How","Bob64"},{"How many","Bob64"},{"Can","Bob64"},{"Can't","Bob64"},{"Want","Bob64"},
-			{"Have","Bob64"},{"Get","Bob64"},{"Will","Bob64"},{"Take","Bob64"},{"Need","Bob64"},{"Not","Bob64"},{"Or","Bob64"},{"And","Bob64"},{"For","Bob64"},{"At","Bob64"}}
-		/* temp comment as other lessons do not have any placeholder motion data
+			{"Have","Bob64"},{"Get","Bob64"},{"Will","Bob64"},{"Take","Bob64"},{"Need","Bob64"},{"Not","Bob64"},{"Or","Bob64"},{"And","Bob64"},{"For","Bob64"},{"At","Bob64"}},
 		new string[,]{//lesson 3 (Common)
 			{"Teach","GT4tube"},{"Learn","GT4tube"},{"Person","GT4tube"},{"Student","GT4tube"},{"Teacher","GT4tube"},{"Friend","GT4tube"},{"Sign","GT4tube"},{"Language","GT4tube"},{"Understand","GT4tube"},
 			{"Know","GT4tube"},{"Don't Know","GT4tube"},{"Be Right Back (BRB)","GT4tube"},{"Accept","GT4tube"},{"Denied","GT4tube"},{"Name","GT4tube"},{"New","GT4tube"},{"Old","GT4tube"},{"Very","GT4tube"},
 			{"Jokes","GT4tube"},{"Funny","GT4tube"},{"Play","GT4tube"},{"Favorite","GT4tube"},{"Draw (pencil)","GT4tube"},{"Stop","GT4tube"},{"Read","GT4tube"},{"Make","GT4tube"},{"Write","GT4tube"},
 			{"Again / Repeat","GT4tube"},{"Slow","GT4tube"},{"Fast","GT4tube"},{"Rude","GT4tube"},{"Eat","GT4tube"},{"Drink","GT4tube"},{"Watch","GT4tube"},{"Work","GT4tube"},{"Live","GT4tube"},{"Play Game","GT4tube"},{"Same","GT4tube"},{"All Right","GT4tube"},{"People","GT4tube"},
-			{"Browsing the Internet","GT4tube"},{"Movie","GT4tube"}
-		} 
+			{"Browsing the Internet","GT4tube"},{"Movie","GT4tube"}},
+		new string[,]{	//lesson 4 (People)
+			{"Family","No Data Yet."},{"Boy","No Data Yet."},{"Girl","No Data Yet."},{"Brother","No Data Yet."},{"Sister","No Data Yet."},{"Brother-in-law","No Data Yet."},{"Sister-in-law","No Data Yet."},
+			{"Father","No Data Yet."},{"Grandpa","No Data Yet."},{"Mother","No Data Yet."},{"Grandma","No Data Yet."},{"Baby","No Data Yet."},{"Child","No Data Yet."},{"Teen","No Data Yet."},
+			{"Adult","No Data Yet."},{"Aunt","No Data Yet."},{"Uncle","No Data Yet."},{"Stranger","No Data Yet."},{"Acquaintance","No Data Yet."},{"Parents","No Data Yet."},{"Born","No Data Yet."},
+			{"Dead","No Data Yet."},{"Marriage","No Data Yet."},{"Divorce","No Data Yet."},{"Single","No Data Yet."},{"Young","No Data Yet."},{"Old","No Data Yet."},{"Age","No Data Yet."},
+			{"Birthday","No Data Yet."},{"Celebrate","No Data Yet."},{"Enemy","No Data Yet."},{"Interpreter","No Data Yet."},{"No one","No Data Yet."},{"Anyone","No Data Yet."},{"Someone","No Data Yet."},
+			{"Everyone","No Data Yet."}},
+		new string[,]{	//lesson 5 (Feelings / Reactions)
+			{"Like","No Data Yet."},{"Hate","No Data Yet."},{"Fine","No Data Yet."},{"Tired","No Data Yet."},{"Sleepy","No Data Yet."},{"Confused","No Data Yet."},{"Smart","No Data Yet."},
+			{"Attention / Focus","No Data Yet."},{"Nevermind","No Data Yet."},{"Angry","No Data Yet."},{"Laughing","No Data Yet."},{"LOL","No Data Yet."},{"Curious","No Data Yet."},{"In Love","No Data Yet."},
+			{"Awesome","No Data Yet."},{"Great","No Data Yet."},{"Nice","No Data Yet."},{"Cute","No Data Yet."},{"Feel","No Data Yet."},{"Pity","No Data Yet."},{"Envy","No Data Yet."},{"Hungry","No Data Yet."},
+			{"Alive","No Data Yet."},{"Bored","No Data Yet."},{"Cry","No Data Yet."},{"Happy","No Data Yet."},{"Sad","No Data Yet."},{"Suffering","No Data Yet."},{"Surprised","No Data Yet."},{"Careful","No Data Yet."},
+			{"Enjoy","No Data Yet."},{"Disgusted","No Data Yet."},{"Embarassed","No Data Yet."},{"Shy","No Data Yet."},{"Lonely","No Data Yet."},{"Stressed","No Data Yet."},{"Scared","No Data Yet."},
+			{"Excited","No Data Yet."},{"Shame","No Data Yet."},{"Struggle","No Data Yet."},{"Friendly","No Data Yet."},{"Mean","No Data Yet."}},
+		new string[,]{	//lesson 6 (Value)
+			{"More","No Data Yet."},{"Less","No Data Yet."},{"Big","No Data Yet."},{"Small","No Data Yet."},{"Full","No Data Yet."},{"Empty","No Data Yet."},{"Half","No Data Yet."},{"Quarter","No Data Yet."},
+			{"Long","No Data Yet."},{"Short","No Data Yet."},{"A little / Small","No Data Yet."},{"A lot / Many","No Data Yet."},{"Unlimited","No Data Yet."},{"Limited","No Data Yet."},{"All","No Data Yet."},
+			{"Nothing","No Data Yet."},{"Ever","No Data Yet."},{"Everything","No Data Yet."},{"Everytime","No Data Yet."},{"Always","No Data Yet."},{"Often","No Data Yet."},{"Sometimes","No Data Yet."},
+			{"Heavy","No Data Yet."},{"Lightweight","No Data Yet."},{"Hard","No Data Yet."},{"Soft","No Data Yet."},{"Strong","No Data Yet."},{"Weak","No Data Yet."},{"First","No Data Yet."},
+			{"Second","No Data Yet."},{"Third","No Data Yet."},{"Next","No Data Yet."},{"Last","No Data Yet."},{"Before","No Data Yet."},{"After","No Data Yet."},{"Busy","No Data Yet."},{"Free","No Data Yet."},
+			{"High","No Data Yet."},{"Low","No Data Yet."},{"Fat","No Data Yet."},{"Thin","No Data Yet."},{"Value","No Data Yet."}},
+		new string[,]{	//lesson 7 (time)
+			{"Time","No Data Yet."},{"Year","No Data Yet."},{"Season","No Data Yet."},{"Month","No Data Yet."},{"Week","No Data Yet."},{"Day","No Data Yet."},{"Weekend","No Data Yet."},{"Hours","No Data Yet."},
+			{"Minutes","No Data Yet."},{"Seconds","No Data Yet."},{"Today","No Data Yet."},{"Tomorrow","No Data Yet."},{"Yesterday","No Data Yet."},{"Morning","No Data Yet."},{"Afternoon","No Data Yet."},
+			{"Evening","No Data Yet."},{"Night","No Data Yet."},{"Sunrise","No Data Yet."},{"Sunset","No Data Yet."},{"All night","No Data Yet."},{"All Day","No Data Yet."},{"Sunday","No Data Yet."},
+			{"Monday","No Data Yet."},{"Tuesday","No Data Yet."},{"Wednesday","No Data Yet."},{"Thursday","No Data Yet."},{"Friday","No Data Yet."},{"Saturday","No Data Yet."},{"Autumn (Fall)","No Data Yet."},
+			{"Winter","No Data Yet."},{"Spring","No Data Yet."},{"Summer","No Data Yet."},{"Now","No Data Yet."},{"Never","No Data Yet."},{"Soon","No Data Yet."},{"Later","No Data Yet."},{"Past","No Data Yet."},
+			{"Future","No Data Yet."},{"Earlier","No Data Yet."},{"Midweek","No Data Yet."},{"Next Week","No Data Yet."},{"Late Afternoon","No Data Yet."}},
+		new string[,]{	//lesson 8 (vrchat)
+			{"Gestures","No Data Yet."},{"World","No Data Yet."},{"Record","No Data Yet."},{"Discord","No Data Yet."},{"Streaming","No Data Yet."},{"Headset (VR)","No Data Yet."},{"Desktop","No Data Yet."},
+			{"Computer","No Data Yet."},{"Instance","No Data Yet."},{"Public","No Data Yet."},{"Invite","No Data Yet."},{"Private","No Data Yet."},{"Add friend","No Data Yet."},{"Menu","No Data Yet."},
+			{"Recharge","No Data Yet."},{"Visit","No Data Yet."},{"Request","No Data Yet."},{"Login","No Data Yet."},{"Logout","No Data Yet."},{"Schedule","No Data Yet."},{"Event","No Data Yet."},
+			{"Online","No Data Yet."},{"Offline","No Data Yet."},{"Cancel","No Data Yet."},{"Portal","No Data Yet."},{"Camera","No Data Yet."},{"Avatar","No Data Yet."},{"Photo","No Data Yet."},
+			{"Join","No Data Yet."},{"Leave","No Data Yet."},{"Climbing","No Data Yet."},{"Falling","No Data Yet."},{"Walk","No Data Yet."},{"Hide","No Data Yet."},{"Block","No Data Yet."},
+			{"Crash","No Data Yet."},{"Lagging","No Data Yet."},{"Restart","No Data Yet."},{"Send","No Data Yet."},{"Receive","No Data Yet."},{"Security","No Data Yet."},{"Donation","No Data Yet."}
+		},
+		new string[,]{	//lesson 9 (Alphabet / Numbers)
+			{"Alphabet / Numbers","No Data Yet."},{"A","No Data Yet."},{"B","No Data Yet."},{"C","No Data Yet."},{"D","No Data Yet."},{"E","No Data Yet."},{"F","No Data Yet."},{"G","No Data Yet."},
+			{"H","No Data Yet."},{"I","No Data Yet."},{"J","No Data Yet."},{"K","No Data Yet."},{"L","No Data Yet."},{"M","No Data Yet."},{"N","No Data Yet."},{"O","No Data Yet."},{"P","No Data Yet."},
+			{"Q","No Data Yet."},{"R","No Data Yet."},{"S","No Data Yet."},{"T","No Data Yet."},{"U","No Data Yet."},{"V","No Data Yet."},{"W","No Data Yet."},{"X","No Data Yet."},{"Y","No Data Yet."},
+			{"Z","No Data Yet."},{"0","No Data Yet."},{"1","No Data Yet."},{"2","No Data Yet."},{"3","No Data Yet."},{"4","No Data Yet."},{"5","No Data Yet."},{"6","No Data Yet."},{"7","No Data Yet."},
+			{"8","No Data Yet."},{"9","No Data Yet."},{"10","No Data Yet."},{"100","No Data Yet."},{"1000","No Data Yet."},{"1000000","No Data Yet."},{"Comma","No Data Yet."},{"Space","No Data Yet."}
+		},
+		new string[,]{	//lesson 10 (Verbs & Actions p1)
+			{"Overlook","No Data Yet."},{"Punish","No Data Yet."},{"Edit","No Data Yet."},{"Erase","No Data Yet."},{"Write","No Data Yet."},{"Proposal","No Data Yet."},{"Add","No Data Yet."},
+			{"Remove","No Data Yet."},{"Agree","No Data Yet."},{"Disagree","No Data Yet."},{"Admit","No Data Yet."},{"Allow","No Data Yet."},{"Attack","No Data Yet."},{"Fight","No Data Yet."},
+			{"Defend","No Data Yet."},{"Defeat","No Data Yet."},{"Win","No Data Yet."},{"Lose","No Data Yet."},{"Draw (game)","No Data Yet."},{"Give up","No Data Yet."},{"Skip","No Data Yet."},
+			{"Ask","No Data Yet."},{"Attach","No Data Yet."},{"Assist","No Data Yet."},{"Bait","No Data Yet."},{"Battle","No Data Yet."},{"Beat","No Data Yet."},{"Become","No Data Yet."},
+			{"Beg","No Data Yet."},{"Begin","No Data Yet."},{"Behave","No Data Yet."},{"Believe","No Data Yet."},{"Blame","No Data Yet."},{"Blow","No Data Yet."},{"Blush","No Data Yet."},{"Bother","No Data Yet."}
+		},
+		new string[,]{	//lesson 11 (Verbs & Actions p2)
+			{"Bend","No Data Yet."},{"Bow","No Data Yet."},{"Break","No Data Yet."},{"Breathe","No Data Yet."},{"Bring","No Data Yet."},{"Build","No Data Yet."},{"Bully","No Data Yet."},{"Burn","No Data Yet."},
+			{"Buy","No Data Yet."},{"Call","No Data Yet."},{"Cancel","No Data Yet."},{"Care","No Data Yet."},{"Carry","No Data Yet."},{"Catch","No Data Yet."},{"Cause","No Data Yet."},{"Challenge","No Data Yet."},
+			{"Chance","No Data Yet."},{"Cheat","No Data Yet."},{"Check","No Data Yet."},{"Choose","No Data Yet."},{"Claim","No Data Yet."},{"Clean","No Data Yet."},{"Clear","No Data Yet."},{"Close","No Data Yet."},
+			{"Comfort","No Data Yet."},{"Command","No Data Yet."},{"Communicate","No Data Yet."},{"Compare","No Data Yet."},{"Complain","No Data Yet."},{"Compliment","No Data Yet."},{"Concentrate","No Data Yet."},
+			{"Construct","No Data Yet."},{"Control","No Data Yet."},{"Cook","No Data Yet."},{"Copy","No Data Yet."},{"Correct","No Data Yet."}
+		},
+		new string[,]{	//lesson 12 (Verbs & Actions p3)
+			{"Cough","No Data Yet."},{"Count","No Data Yet."},{"Create","No Data Yet."},{"Cuddle","No Data Yet."},{"Cut","No Data Yet."},{"Dab","No Data Yet."},{"Dance","No Data Yet."},{"Dare","No Data Yet."},
+			{"Date","No Data Yet."},{"Deal","No Data Yet."},{"Deliver","No Data Yet."},{"Depend","No Data Yet."},{"Describe","No Data Yet."},{"Dirty","No Data Yet."},{"Disappear","No Data Yet."},
+			{"Disappoint","No Data Yet."},{"Disapprove","No Data Yet."},{"Discuss","No Data Yet."},{"Disguise","No Data Yet."},{"Disgust","No Data Yet."},{"Dismiss","No Data Yet."},{"Disturb","No Data Yet."},
+			{"Doubt","No Data Yet."},{"Dream","No Data Yet."},{"Dress","No Data Yet."},{"Drunk","No Data Yet."},{"Drop","No Data Yet."},{"Drown","No Data Yet."},{"Dry","No Data Yet."},{"Dump","No Data Yet."},
+			{"Dust","No Data Yet."},{"Earn","No Data Yet."},{"Effect","No Data Yet."},{"End","No Data Yet."},{"Escape","No Data Yet."},{"Escort","No Data Yet."}
+		},
+		new string[,]{	//lesson 13 (Verbs & Actions p4)
+			{"Excuse","No Data Yet."},{"Expose","No Data Yet."},{"Exist","No Data Yet."},{"Fail","No Data Yet."},{"Faint","No Data Yet."},{"Fake","No Data Yet."},{"Fart","No Data Yet."},{"Fear","No Data Yet."},
+			{"Fill","No Data Yet."},{"Find","No Data Yet."},{"Finish","No Data Yet."},{"Fix","No Data Yet."},{"Flip","No Data Yet."},{"Flirt","No Data Yet."},{"Fly","No Data Yet."},{"Forbid","No Data Yet."},
+			{"Forgive","No Data Yet."},{"Gain","No Data Yet."},{"Give","No Data Yet."},{"Glow","No Data Yet."},{"Grab","No Data Yet."},{"Grow","No Data Yet."},{"Guard","No Data Yet."},{"Guess","No Data Yet."},
+			{"Guide","No Data Yet."},{"Harass","No Data Yet."},{"Harm","No Data Yet."},{"Hit","No Data Yet."},{"Hold","No Data Yet."},{"Hop","No Data Yet."},{"Hope","No Data Yet."},{"Hunt","No Data Yet."},
+			{"Ignore","No Data Yet."},{"Imagine","No Data Yet."},{"Imitate","No Data Yet."},{"Insult","No Data Yet."}
+		},
+		new string[,]{	//lesson 14 (Verbs & Actions p5)
+			{"Interact","No Data Yet."},{"Interfere","No Data Yet."},{"Judge","No Data Yet."},{"Jump","No Data Yet."},{"Justify","No Data Yet."},{"Just kidding","No Data Yet."},{"Keep","No Data Yet."},
+			{"Kick","No Data Yet."},{"Kill","No Data Yet."},{"Knock","No Data Yet."},{"Lead","No Data Yet."},{"Lick","No Data Yet."},{"Lock","No Data Yet."},{"Manipulate","No Data Yet."},{"Melt","No Data Yet."},
+			{"Mess","No Data Yet."},{"Miss","No Data Yet."},{"Mistake","No Data Yet."},{"Mount","No Data Yet."},{"Move","No Data Yet."},{"Murder","No Data Yet."},{"Nod","No Data Yet."},{"Note","No Data Yet."},
+			{"Notice","No Data Yet."},{"Obey","No Data Yet."},{"Obsess","No Data Yet."},{"Obtain","No Data Yet."},{"Occupy","No Data Yet."},{"Offend","No Data Yet."},{"Offer","No Data Yet."},{"Okay","No Data Yet."},
+			{"Open","No Data Yet."},{"Order","No Data Yet."},{"Owe","No Data Yet."},{"Own","No Data Yet."},{"Pass","No Data Yet."}
+		},
+		new string[,]{	//lesson 15 (Verbs & Actions p6)
+			{"Pat","No Data Yet."},{"Party","No Data Yet."},{"Pet","No Data Yet."},{"Pick","No Data Yet."},{"Plug","No Data Yet."},{"Point","No Data Yet."},{"Poke","No Data Yet."},{"Pray","No Data Yet."},
+			{"Prepare","No Data Yet."},{"Present","No Data Yet."},{"Pretend","No Data Yet."},{"Protect","No Data Yet."},{"Prove","No Data Yet."},{"Publish","No Data Yet."},{"Puke","No Data Yet."},{"Pull","No Data Yet."},
+			{"Punch","No Data Yet."},{"Put","No Data Yet."},{"Push","No Data Yet."},{"Question","No Data Yet."},{"Quit","No Data Yet."},{"Quote","No Data Yet."},{"Race","No Data Yet."},{"React","No Data Yet."},
+			{"Recommended","No Data Yet."},{"Refuse","No Data Yet."},{"Regret","No Data Yet."},{"Remember","No Data Yet."},{"Replace","No Data Yet."},{"Report","No Data Yet."},{"Reset","No Data Yet."},{"Ride","No Data Yet."},
+			{"Rub","No Data Yet."},{"Rule","No Data Yet."},{"Run","No Data Yet."},{"Save","No Data Yet."}
+		},
+		new string[,]{	//lesson 16 (Verbs & Actions p7)
+		{"Say","No Data Yet."},{"Search","No Data Yet."},{"See","No Data Yet."},{"Share","No Data Yet."},{"Shock","No Data Yet."},{"Shop","No Data Yet."},{"Show","No Data Yet."},{"Shut up","No Data Yet."},
+{"Shut down","No Data Yet."},{"Sing","No Data Yet."},{"Sit","No Data Yet."},{"Smell","No Data Yet."},{"Smile","No Data Yet."},{"Smoke","No Data Yet."},{"Speak","No Data Yet."},{"Spell","No Data Yet."},
+{"Spit","No Data Yet."},{"Stand","No Data Yet."},{"Start","No Data Yet."},{"Stay","No Data Yet."},{"Steal","No Data Yet."},{"Stop","No Data Yet."},{"Study","No Data Yet."},{"Suffer","No Data Yet."},
+{"Swim","No Data Yet."},{"Switch","No Data Yet."},{"Take","No Data Yet."},{"Talk","No Data Yet."},{"Tell","No Data Yet."},{"Test","No Data Yet."},{"Text","No Data Yet."},{"Think","No Data Yet."},
+{"Throw","No Data Yet."},{"Tie","No Data Yet."},{"Truth","No Data Yet."},{"Try","No Data Yet."}
+		}/*,
+		new string[,]{	//lesson 17 (Verbs & Actions p8)
+		},
+		new string[,]{	//lesson 18 (Food)
+		},
+		new string[,]{	//lesson 19 (Places)
+		},
+		new string[,]{	//lesson 20 (Stuff / Weather)
+		},
+		new string[,]{	//lesson 21 (Clothes / Equipment)
+		},
+		new string[,]{	//lesson 22 (Fantasy / Characters)
+		},
+		new string[,]{	//lesson 23 (Holidays / Special Days)
+		},
+		new string[,]{	//lesson 24 (Home stuff)
+		},
+		new string[,]{	//lesson 25 (Nature / Environment)
+		},
+		new string[,]{	//lesson 26 (Talk / Asking exercises)
+		},
+		new string[,]{	//lesson 27 (Name sign users)
+		},
+		new string[,]{	//lesson 27 (Countries)
+		},
+		new string[,]{	//lesson 27 (Colors)
+		},
+		new string[,]{	//lesson 27 (Medical)
+*/
 		//other lessons to be added below here
-		*/
 		};
 		string [] ASLlessonnames = new string[]{//array of ASL (and possibilly other language) lesson names - can be unique per language.
 		"Daily Use","Pointing use Question/Answer","Common","People","Feelings / Reactions","Value","Time","VRChat","Alphabet / Numbers","Verbs & Actions p1","Verbs & Actions p2","Verbs & Actions p3",
@@ -139,7 +256,7 @@ public class CreateASLButtons : MonoBehaviour {
 					createlessonboard(aslroot,ASLlessons[x],ASLlessonnames[x],"ASL",x,asllessonmenu,rootcanvas);//Loops and creates lesson boards for all initialized lessons in the lessonarray.
 				}
 				
-				 createmenu(aslroot,"ASL",ASLlessonnames,asllessonmenu,langselectmenu); //creates the lesson chooser menu
+				 createmenu(aslroot,"ASL",ASLlessonnames,ASLlessons,asllessonmenu,langselectmenu); //creates the lesson chooser menu
 				//createlessonbackmenu(aslroot,"ASL"); //language issue. Does the triggers find all languages? Back menu all the way back to language select? Another back menu once on main menu to get to language select?
 				createlanguageselect(langselectmenu,signlanguages); //creates main language selection menu
 				aslvideocontainer.SetActive(false);
@@ -296,7 +413,7 @@ go.SetActive(false);
 	Navigation no_nav = new Navigation();
 	no_nav.mode=Navigation.Mode.None; 
 
-	GameObject menuheaderresourcesuitext = createheadertext(parent,"VR Sign Language Select Menu");//Add menu header text:
+	createheadertext(parent,"VR Sign Language Select Menu");//Add menu header text:
 	//for (int x = 0; x < signlanguages.GetLength(0); x++){
 		//Debug.Log(signlanguages.GetLength(0) + " " +x);
 		int x=0;
@@ -388,18 +505,18 @@ static GameObject FindInActiveObjectByName(string name)
 
 
 
-static GameObject createmenu(GameObject parent, string lang, string[] lessonname, GameObject menu, GameObject langselectmenu){	
+static GameObject createmenu(GameObject parent, string lang, string[] lessonname,string[][,] ASLlessons, GameObject menu, GameObject langselectmenu){	
 			Navigation no_nav = new Navigation();
 		no_nav.mode=Navigation.Mode.None; 
 
 	//GameObject lessonselectcanvas = createandreturncanvas("Lesson Select - " + lang,menuroot); //Create lesson select gameobject. 
 	//GameObject menucanvaspanel= createpanel(lessonselectcanvas); //create panel background
-	GameObject menuheaderresourcesuitext = createheadertext(menu,"VR-"+lang+" Sign Language - Lesson Menu");//Add menu header text:
+	createheadertext(menu,"VR-"+lang+" Sign Language - Lesson Menu");//Add menu header text:
 
 		int column = 0;
 		int row = 0;
-		//for (int x = 0; x < lessonname.GetLength(0); x++) //this is the main loop that processes the array and creates + organizes the buttons in rows+columns.
-		for (int x = 0; x < 2; x++)
+		for (int x = 0; x < ASLlessons.Length; x++) //this is the main loop that processes the array and creates + organizes the buttons in rows+columns.
+		//for (int x = 0; x < 2; x++)
 		{
 			if(x != 0){
 				if (x % 9 == 0){ //display 9 items per column
